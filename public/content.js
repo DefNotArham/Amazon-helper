ab5be655252ee92e52f2d05888b3e64b512ea11a
DefNotArham <arhamkabir231@gmail.com>
2026-09-02 18:43:09 -0400

    Update content.js

--- public/content.js
+++ public/content.js
@@ -1,10 +1,26 @@
 const isAmazonProduct = () => {
   const url = window.location.href;
+  const hostname = window.location.hostname;
+  let result = {
+    isProduct: false,
+    asin: "",
+  };
 
-  console.log("Current URL", url);
-  console.log("Has /dp/:", url.includes("/dp/"));
+  if (hostname !== "www.amazon.com") {
+    console.log("This is not an amazon url");
+    return result;
+  }
 
-  return url.includes("amazon.com/dp/");
+  if (!url.includes("/dp/")) return result;
+
+  const parts = url.split("/dp/");
+  const asin = parts[1].split("?")[0].split("/")[0];
+
+  result.isProduct = true;
+  result.asin = asin;
+
+  console.log(result);
+  return result;
 };
 
 console.log("Is product:", isAmazonProduct());
